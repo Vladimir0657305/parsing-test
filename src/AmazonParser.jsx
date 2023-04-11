069108d70600deeea53fce18d6d2eda32e8f43e5
Vladimir0657305 <vv0657305@gmail.com>
2023-04-11 13:10:39 +0300

    changes

--- src/AmazonParser.jsx
+++ src/AmazonParser.jsx
@@ -5,30 +5,39 @@ import { CSVReader, CSVDownloader } from 'react-papaparse';
 
 
 function AmazonParser() {
+    let paginator = '';
+    let products = [];
     const [searchTerm, setSearchTerm] = useState('');
 
 
     const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
     const SEARCH_URL = `https://www.amazon.com/s?k=`;
+    const NEXT_URL = `https://www.amazon.com`;
+
 
     const handleSearchSubmit = async (event) => {
         event.preventDefault();
-        const response = await axios.get(`${PROXY_URL}${SEARCH_URL}${searchTerm}`);
-        const products = parseProducts(response.data);
+        if (paginator) {
+            let response = await axios.get(`${PROXY_URL}${SEARCH_URL}${searchTerm}`);
+            products = parseProducts(response.data);
+            // downloadCsv(products);
+        } else if (paginator ) {
+            const delayTime = Math.floor(Math.random() * (max - min + 1)) + min;
+            setTimeout(() => {
+                let response = axios.get(`${PROXY_URL}${NEXT_URL}${paginator}`);
+                products = parseProducts(response.data);
+                // downloadCsv(products);
+            }, delayTime);
+        } else {
+            downloadCsv(products);
+        }
+
         // console.log(products);
-        downloadCsv(products);
+
     };
 
     const handleSearchTermChange = (event) => {
         setSearchTerm(event.target.value);
-    };
-
-    const downloadCsv = (products) => {
-        const csv = Papa.unparse(products);
-        const downloadLink = document.createElement('a');
-        downloadLink.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv));
-        downloadLink.setAttribute('download', 'amazon-products.csv');
-        downloadLink.click();
     };
 
     const parseProducts = (html) => {
@@ -37,8 +46,7 @@ function AmazonParser() {
         // console.log(doc);
         const products = [];
 
-
-        const paginator = doc.querySelector('li.a-last > a');
+        paginator = doc.querySelector('li.a-last > a');
         console.log('paginator=', paginator?.href);
 
         doc.querySelectorAll('div[data-component-type="s-search-result"]').forEach((item) => {
@@ -63,6 +71,15 @@ function AmazonParser() {
         return products;
     };
 
+    // setSearchTerm(paginator);
+
+    const downloadCsv = (products) => {
+        const csv = Papa.unparse(products);
+        const downloadLink = document.createElement('a');
+        downloadLink.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv));
+        downloadLink.setAttribute('download', 'amazon-products.csv');
+        downloadLink.click();
+    };
 
 
     return (
